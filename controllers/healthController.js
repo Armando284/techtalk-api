67a9241a05a49712aabc4dedfbb15087cdfcc05b
Armando <72764913+Armando284@users.noreply.github.com>
2024-10-25 00:38:47 -0400

    Feat: Adds health route to check server and database connection status.
    Create health check route. #2

---
+++ controllers/healthController.js
@@ -0,0 +1,21 @@
+const { sequelize } = require('../models');
+
+async function checkDatabaseConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection to the database has been established successfully.');
+    return { status: 'ok', database: 'connected' };
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+    return { status: 'error', database: 'disconnected', error: error.message };
+  }
+}
+
+exports.checkHealth = async (req, res, next) => {
+  try {
+    const response = await checkDatabaseConnection()
+    res.json(response)
+  } catch (error) {
+    res.status(500).json({ error: 'Server check health error!' })
+  }
+}
