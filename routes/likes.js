22f2efa730ece170b73daf54bbbc5232e3f98e2e
Armando <72764913+Armando284@users.noreply.github.com>
2024-11-20 18:27:58 -0500

    refactor: Removes all the code to start by railway migration

--- routes/likes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const likeController = require('../controllers/likeController')
-
-router.get('/', likeController.getAllLikes)
-
-module.exports = router
