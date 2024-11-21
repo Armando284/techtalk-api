5db3fbae0d6ff416134e2cb44f7f81c9f3300dab
Armando <72764913+Armando284@users.noreply.github.com>
2024-11-21 14:38:51 -0500

    fix: Sets single name for database env variable

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   development: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_DEV,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: console.log,
@@ -12,14 +12,14 @@ module.exports = {
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_TEST,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
   },
   production: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME_PROD,
+    database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'postgres',
     logging: false, // Deactivate logging on production
